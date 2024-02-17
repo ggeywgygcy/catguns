28ebfe234fdcae94c54bce7408fc57a0673d7e63
ggeywgygcy <wasilij18bus@gmail.com>
2024-02-17 17:55:54 +0300

    создал болванку

---
+++ src/components/Addjob.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="rows">
+  <h1>принятие на работу</h1>  
+    <div>
+      <div class="mb-3">
+        <label for="CatName" class="form-label">кличка</label>
+        <input type="text" class="form-control" id="CatName" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="CatAge" class="form-label">скок лет</label>
+        <input type="password" class="form-control" id="CatAge">
+      </div>
+  
+  <button type="submit" class="btn btn-primary">подать заяву</button>
+</div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Addjod',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Addjob from '@/components/Addjob.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/addjob',
+    name: 'addjob',
+    component: Addjob
   },
   {
     path: '/about',
